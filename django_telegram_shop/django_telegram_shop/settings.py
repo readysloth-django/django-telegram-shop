e731748923fcb1a372d5c2f35ec795310a2cfb7f
readysloth <readysloth@protonmail.com>
2024-06-23 19:53:33 +0300

    fix: Added `STATIC_ROOT`

--- django_telegram_shop/django_telegram_shop/settings.py
+++ django_telegram_shop/django_telegram_shop/settings.py
@@ -123,6 +123,7 @@ class Base(Configuration):
     # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
     STATIC_URL = 'static/'
+    STATIC_ROOT = 'static'
 
     # Default primary key field type
     # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
