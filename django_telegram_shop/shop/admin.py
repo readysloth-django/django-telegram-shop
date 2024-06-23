71a5e80203af512b99094be298082aa84a5fe7a7
readysloth <readysloth@protonmail.com>
2024-06-23 19:29:53 +0300

    feat: Registered models in admin-panel

--- django_telegram_shop/shop/admin.py
+++ django_telegram_shop/shop/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import (
+    Buyer,
+    SaleVerificationType,
+    SaleVerification,
+    Cart,
+    ProductCategory,
+    Product
+)
+
+admin.site.register(Buyer)
+admin.site.register(SaleVerificationType)
+admin.site.register(SaleVerification)
+admin.site.register(Cart)
+admin.site.register(ProductCategory)
+admin.site.register(Product)
